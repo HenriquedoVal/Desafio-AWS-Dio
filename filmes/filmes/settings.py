1925971386edfaa64c7f4ed2094f86c09c74a52d
Henrique do Val <87449272+HenriquedoVal@users.noreply.github.com>
2022-04-12 00:31:19 -0300

    Update settings.py

--- filmes/filmes/settings.py
+++ filmes/filmes/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w^qid&gx@7#s+xx2-6#36(p%)ou3+hkxgfqd3-ejx95y-pb4h='
+SECRET_KEY = '' #<--
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
